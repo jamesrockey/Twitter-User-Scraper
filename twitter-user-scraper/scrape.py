641ce3a04e81d9a9759d3fe654a1f554e4a6b529
James Rockey <jrockey2@illinois.edu>
2022-01-03 13:52:01 -0600

    Added more properties to user class

--- twitter-user-scraper/scrape.py
+++ twitter-user-scraper/scrape.py
@@ -6,15 +6,12 @@ def configure_api():
     while True:
         print("Please enter the following information to access Twitter API")
         try:
-            # consumer_key = input("Please enter Consumer Key \n").strip()
-            # consumer_secret = input("Please enter Consumer Secret Key \n").strip()
-            # access_token = input("Please enter Access Token \n").strip()
-            # access_token_secret = input("Please enter Access Token Secret \n").strip()
+            consumer_key = input("Please enter Consumer Key \n").strip()
+            consumer_secret = input("Please enter Consumer Secret Key \n").strip()
+            access_token = input("Please enter Access Token \n").strip()
+            access_token_secret = input("Please enter Access Token Secret \n").strip()
 
-            consumer_key = "Kjc7UDsZHzWfd4deChQC93jYz"
-            consumer_secret = "CzQ5yzUSHpMin7cSUPWOGzOiKyLRxPgHg9zPUohHMQ8jBGl7Mz"
-            access_token = "1475541055084937219-oDLFr86cjzrj1Uwf2CQQhg3r0slKSa"
-            access_token_secret = "xwdxwlx28W9piyrt1QHKtmKDmchRQjdnh06yWBNAF1W5U"
+
 
             auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
@@ -58,22 +55,20 @@ def create_user(api, username):
         return
     else:
         new_user = User(username)
+        new_user.id_str = user.id_str
         new_user.name = user.name
         new_user.created_at = user.created_at
-
-        try: # api throws exception for is private variable only
-            new_user.tweets_is_private = user.tweets_is_private
-        except:
-            new_user.tweets_is_private = False
-        else:
-            new_user.tweets_is_private = True
-        new_user.location = user.location
-        print("e")
-        new_user.description = user.description
-        print("f")
+        new_user.protected = user.protected
+        # check if user.location, user.description are non empty
+        if user.location:
+            new_user.location = user.location
+        if user.description:
+            new_user.description = user.description
         new_user.verified = user.verified
-        print("g")
-    return new_user
+        new_user.followers_count = user.followers_count
+        new_user.friends_count = user.friends_count
+        new_user.withheld_in_countries = user.withheld_in_countries
+        return new_user
 
 
 if __name__ == "__main__":
--- twitter-user-scraper/user.py
+++ twitter-user-scraper/user.py
@@ -1,16 +1,33 @@
 class User:
     def __init__(self, username):
+        self.id_str = None
         self.username = username
         self.name = None
         self.created_at = None
-        self.tweets_is_private = False
+        self.protected = False
         self.location = None
         self.description = None
         self.verified = False
+        self.followers_count = 0
+        self.friends_count = 0
+        self.withheld_in_countries = []
 
     # returns string information about user to be printed to console
     def __str__(self):
-        lst = ["Username: " + self.username, "Name: " + self.name, "Account created at: " + str(self.created_at),
-               "Tweets_is_private: " + str(self.tweets_is_private), "Location: " + self.location,
-               "Description: " + self.description, "Verified: " + str(self.verified)]
-        return ", ".join(lst)
+        # check for missing location, description, and withheld countries
+        location = self.location
+        description = self.description
+        banned = self.withheld_in_countries
+        if (location == None):
+            location = 'N/A'
+        if (description == None):
+            description = 'N/A'
+        if (len(banned) == 0):
+            banned.append('N/A')
+        lst = ["ID: " + self.id_str, "Username: " + self.username, "Name: " + self.name,
+               "Account created at: " + str(self.created_at),
+               "Tweets_is_private: " + str(self.protected), "Location: " + location,
+               "Description: " + description, "Verified: " + str(self.verified),
+               "Followers count: " + str(self.followers_count), "Friend count: " + str(self.friends_count), "Content withheld in countries: " + ", ".join(str(elem) for elem in banned)]
+
+        return "\n".join(lst)
