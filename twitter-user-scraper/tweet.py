5f1cf19fca01446da2ef00213e032b61e887c4f6
James Rockey <jrockey2@illinois.edu>
2022-01-03 22:49:36 -0600

    Added tweet class

---
+++ twitter-user-scraper/tweet.py
@@ -0,0 +1,24 @@
+class Tweet:
+    def __init__(self, id, text, user, author, favorite_count, retweet_count):
+        self.id = id
+        self.text = text
+        self.user = user
+        self.author = author
+        self.favorite_count = favorite_count
+        self.retweet_count = retweet_count
+        self.url = "https://twitter.com/twitter/statuses/" + id
+        self.is_retweet: bool
+        # check text to see if post is retweet
+        try:
+            if text.find('RT @') == 0:
+                self.is_retweet = True
+        except:
+            self.is_retweet = False
+        else:
+            self.is_retweet = False
+
+    def __str__(self):
+        lst = ["ID: " + self.id, "Text: " + self.text, "Author: " + self.user, "Favorite count: " +
+               str(self.favorite_count), "Retweet count: " + str(self.retweet_count),
+               "Url: " + self.url, "Is Retweet: " + str(self.is_retweet)]
+        return "\n".join(lst)
