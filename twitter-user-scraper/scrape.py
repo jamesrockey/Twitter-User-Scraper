6e0c2b49d8b3b59a220bce865e603025f07901d9
James Rockey <jrockey2@illinois.edu>
2022-01-03 19:58:34 -0600

    Fixed user permission issue

--- twitter-user-scraper/scrape.py
+++ twitter-user-scraper/scrape.py
@@ -7,10 +7,15 @@ def configure_api():
     while True:
         print("Please enter the following information to access Twitter API")
         try:
-            consumer_key = input("Please enter Consumer Key \n").strip()
-            consumer_secret = input("Please enter Consumer Secret Key \n").strip()
-            access_token = input("Please enter Access Token \n").strip()
-            access_token_secret = input("Please enter Access Token Secret \n").strip()
+            # consumer_key = input("Please enter Consumer Key \n").strip()
+            # consumer_secret = input("Please enter Consumer Secret Key \n").strip()
+            # access_token = input("Please enter Access Token \n").strip()
+            # access_token_secret = input("Please enter Access Token Secret \n").strip()
+
+            consumer_key = "Kjc7UDsZHzWfd4deChQC93jYz"
+            consumer_secret = "CzQ5yzUSHpMin7cSUPWOGzOiKyLRxPgHg9zPUohHMQ8jBGl7Mz"
+            access_token = "1475541055084937219-oDLFr86cjzrj1Uwf2CQQhg3r0slKSa"
+            access_token_secret = "xwdxwlx28W9piyrt1QHKtmKDmchRQjdnh06yWBNAF1W5U"
 
             auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
@@ -46,31 +51,18 @@ def create_user(api, username):
         print("Invalid Username please try again")
     else:
         # instantiate new user info
-        new_user = User()
-        new_user.id_str = user.id_str
-        new_user.username = user.screen_name
-        new_user.name = user.name
-        new_user.created_at = user.created_at
-        new_user.protected = user.protected
-        if user.location:  # check if user.location, user.description are non empty
-            new_user.location = user.location
-        if user.description:
-            new_user.description = user.description
-        new_user.verified = user.verified
-        new_user.followers_count = user.followers_count
-        new_user.friends_count = user.friends_count
-        new_user.withheld_in_countries = user.withheld_in_countries
-        # add 20 most recent follower's usernames
-        for friend in user.friends():
-            new_user.recent_friends.append(friend.screen_name)
-        for follower in user.followers():
-            new_user.recent_followers.append(follower.screen_name)
 
-
-        # for friend in tweepy.Cursor(api.get_friends, screen_name=new_user.username).items():
-        #     new_user.friends.append(friend.screen_name)
+        new_user = User(time.asctime(), user.id_str, user.screen_name, user.name, user.created_at, user.protected,
+                        user.location, user.description, user.verified, user.followers_count, user.friends_count,
+                        user.withheld_in_countries)
+        # If the user is not protected, gather list of user's recent followers, friends, and tweets
+        if not new_user.protected:
+            for friend in user.friends():
+                new_user.recent_friends.append(friend.screen_name)
+            for follower in user.followers():
+                new_user.recent_followers.append(follower.screen_name)
+            return new_user
         return new_user
-
 
 if __name__ == "__main__":
     # get valid tweepy api instance
--- twitter-user-scraper/user.py
+++ twitter-user-scraper/user.py
@@ -1,16 +1,18 @@
 class User:
-    def __init__(self):
-        self.id_str = None
-        self.username = None
-        self.name = None
-        self.created_at = None
-        self.protected = False
-        self.location = None
-        self.description = None
-        self.verified = False
-        self.followers_count = 0
-        self.friends_count = 0
-        self.withheld_in_countries = []
+    def __init__(self, scrape_time, id_str, screen_name, name, created_at, protected, location, description, verified,
+                 followers_count, friends_count, withheld_in_countries):
+        self.scrape_time = scrape_time
+        self.id_str = id_str
+        self.username = screen_name
+        self.name = name
+        self.created_at = created_at
+        self.protected = protected
+        self.location = location
+        self.description = description
+        self.verified = verified
+        self.followers_count = followers_count
+        self.friends_count = friends_count
+        self.withheld_in_countries = withheld_in_countries
         self.recent_followers = []
         self.recent_friends = []
 
@@ -22,17 +24,17 @@ class User:
         banned = self.withheld_in_countries
         recent_followers = self.recent_followers
         recent_friends = self.recent_friends
-        if (location == None):
+        if not location:
             location = 'N/A'
-        if (description == None):
+        if not description:
             description = 'N/A'
         if len(banned) == 0:
             banned.append('N/A')
         if self.followers_count == 0:
-            recent_followers.append("User has no followers")
+            recent_followers.append('N/A')
         if self.friends_count == 0:
-            recent_friends.append("User has no friends")
-        lst = ["ID: " + self.id_str, "Username: " + self.username, "Name: " + self.name,
+            recent_friends.append('N/A')
+        lst = ["Time last updated: " + str(self.scrape_time), "ID: " + self.id_str, "Username: " + self.username, "Name: " + self.name,
                "Account created at: " + str(self.created_at),
                "Tweets_is_private: " + str(self.protected), "Location: " + location,
                "Description: " + description, "Verified: " + str(self.verified),
